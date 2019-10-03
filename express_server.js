9dcbede1da5e524c16c04601916fe61171ad80d3
Aiman Azmie <aimanaaw@gmail.com>
2019-10-03 00:12:16 +0000

    Added the login, register and logout features. When a user is logged in, their email is displayed. Logout logs out the user. Register creates a new user. Test have been performed to check if the entered email is existing and in the correct format. Tests have been performed to check if the password entered during login is correct and if a password is entered during registration. Tests have been performed to make sure the logout feature works correctly

--- express_server.js
+++ express_server.js
@@ -2,8 +2,32 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
+const emailLookUp = function(email) {
+  for (let usr in users) {
+    if (email === users[usr].email) {
+      return users[usr];
+    };
+  };
+  return false;
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -22,9 +46,52 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+
 app.get("/urls", (req, res) => {
-  let templateVars = {urls: urlDatabase}
+  let templateVars = {urls: urlDatabase, user: users[req.cookies["user_id"]]}
   res.render("urls_index", templateVars);
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("user_id", req.body.user_id);
+  res.redirect("/login");
+})
+
+app.post("/login", (req, res) => {
+  let userEmail = req.body.email;
+  let potentialUser = emailLookUp(userEmail);
+  if (potentialUser === false) {
+    res.send("Error 403: Email not found")
+  }
+  if (potentialUser.password === req.body.password) {
+    res.cookie("user_id", potentialUser.id);
+    res.redirect("/urls");
+  } else {
+    res.send("Incorrect password")
+  }
+})
+
+app.get("/login", (req, res) => {
+  let templateVars = {urls: urlDatabase, user:users[req.cookies["user_id"]]}
+  res.render("urls_login.ejs", templateVars);
+})
+
+app.post("/registration", (req, res) => {
+  if (req.body.email === "" || req.body.password === "") {
+    res.send("Error 400");
+  };
+  if (!emailLookUp(req.body.email)) {
+    const tempUserID = generateRandomString();
+    users[tempUserID] = {id:tempUserID, email:req.body.email, password:req.body.password};
+    res.cookie("user_id", users[tempUserID])
+    res.redirect("/urls");
+  } else {
+    res.send("Email already exists")
+  }
+});
+app.get("/registration", (req, res) => {
+  let templateVars = {urls: urlDatabase, user:users[req.cookies["user_id"]]}
+  res.render("urls_registrationPage.ejs", templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -40,15 +107,17 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {urls: urlDatabase, user:users[req.cookies["user_id"]]}
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL]
+    longURL: urlDatabase[req.params.shortURL],
+    users
   }
-  res.render("urls_show", templateVars);
+  res.render("urls_show", templateVars, users);
 });
 
 app.post("/urls/:shortURL/edit", (req, res) => {
